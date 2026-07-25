539e189f6634d4294b980c0c2ab610ccea42e973
Logan <iml885203@gmail.com>
2026-07-26 00:43:32 +0800

    feat(site): improve social sharing metadata

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -7,6 +7,7 @@ output=${1:-"$root/_site"}
 
 mkdir -p "$output"
 cp "$root/site/index.html" "$root/site/styles.css" "$root/site/site.js" "$output/"
+cp "$root/site/og-image.png" "$output/og-image.png"
 cp "$root/site/CNAME" "$output/CNAME"
 cp "$root/assets/icon.svg" "$output/icon.svg"
 touch "$output/.nojekyll"
